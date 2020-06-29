5c88924badd478f5fe1e18f41c93ad350ca72620
andooown <andooown.dev@gmail.com>
2020-06-30 00:42:45 +0900

    Add example

---
+++ Example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftURLParseExample
+//
+//  Created by Yoshikazu Ando on 2020/06/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
